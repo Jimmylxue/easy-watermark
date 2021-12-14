aa77df21718b1f77d652e631182c0153c14eca3c
jimmyxuexue <1002661758@qq.com>
2021-12-14 21:10:37 +0800

    水印刷新

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,44 @@
-export function waterMarker(img) {}
+interface config {
+  src: string; // 图片地址
+  text: string; // 水印文字
+}
+export default function imgWaterMarker(src: string, text: string) {
+  return new Promise((resolve, reject) => {
+    let img = new Image();
+    img.setAttribute("crossOrigin", "");
+    img.src = src;
+    console.log(img, "img");
+    console.log(img);
+    img.onload = () => {
+      const width = img.width;
+      const height = img.height;
+      console.log("width:", width, "height", height);
+      const canvas = document.createElement("canvas");
+      canvas.width = width;
+      canvas.height = height;
+      const ctx = canvas.getContext("2d"); // 画笔
+      ctx.drawImage(img, 0, 0, width, height);
+      // console.log(draw, img);
+      // ctx.fillStyle = "#2ecc71";
+      // ctx.fill();
+      // ctx.font = "40px bold italic Courier New";
+      ctx.font = "40px bold italic arial";
+      let lineGradient = ctx.createLinearGradient(100, 200, 200, 200);
+      // lineGradient.addColorStop(0, "#f1c40f");
+      lineGradient.addColorStop(1, "#CCC");
+      console.log("123456");
+      // ctx.font = "bold italic 160px arial";
+      ctx.shadowBlur = 10;
+      ctx.shadowOffsetX = 20;
+      ctx.shadowOffsetY = 20;
+      ctx.fillStyle = lineGradient;
+      // ctx.rotate((350 * Math.PI) / 180);
+      ctx.fillText("Jimmyxuexue", width - 250, height - 100);
+
+      resolve(canvas.toDataURL("image/jpeg"));
+    };
+
+    // 后面两个数值表示从哪里开始画 如这里是从x = 100 y = 200开始画
+    // return canvas.toDataURL("image/jpeg");
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
-const TenserWebpackPlugin = require('terser-webpack-plugin');
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const TenserWebpackPlugin = require("terser-webpack-plugin");
 /**
  * 在个人电脑 使用 npm login 先登录之后  包文件写好了之后就可以使用 npm publish  发布这个包了
  *  包的名称就是 package.json 中的name字段
@@ -9,16 +9,16 @@ const TenserWebpackPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: {
-    'add-number': './src/main.ts',
-    'add-number.min': './src/main.ts',
+    watermark: "./src/main.ts",
+    "watermark.min": "./src/main.ts",
   },
   output: {
-    filename: '[name].js',
+    filename: "[name].js",
     /**
      * 使用 library 这个字段定义打包库之后的名称，如这里的名称是 addNumber 这个库默认导出的一个相加的方法
      *  在使用开发的时候就使用  addNumber(10,20) 这样就可以了
      */
-    library: 'addNumber',
+    library: "waterMarker",
     /**
      * libraryTarget 有很值，让打包之后可以有很多的规范 比如 var this global 等等
      * 最终打包的效果也就是如：
@@ -27,22 +27,22 @@ module.exports = {
      *
      * umd是一种标准的格式，可以兼容其他的  推荐这个一般都使用 umd
      */
-    libraryTarget: 'umd', //umd 是兼容的
+    libraryTarget: "umd", //umd 是兼容的
     /**
      * 包的导出默认是模块导出，结果是 Module {__esModule: true, Symbol(Symbol.toStringTag): 'Module'}
      * 模块导出的内容在.default,  使用这个字段可以自动我们只获取.default 中的内容
      */
-    libraryExport: 'default',
+    libraryExport: "default",
   },
   resolve: {
-    extensions: ['.js', '.json', '.ts'],
+    extensions: [".js", ".json", ".ts"],
   },
   module: {
     rules: [
       {
         test: /\.ts/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
     ],
@@ -51,7 +51,7 @@ module.exports = {
    * 因为需要打包出来一份压缩版本和未压缩版本，所以设置mode就没用了，
    * 所以这里使用专门打包压缩用的插件 terser-webpack-plugin
    */
-  mode: 'none',
+  mode: "none",
   /**
    * optimization webpack专门用来做优化的一个字段
    */
@@ -65,15 +65,16 @@ module.exports = {
   },
   plugins: [
     new htmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-      scriptLoading: 'blocking',
+      template: "./index.html",
+      filename: "index.html",
+      scriptLoading: "blocking",
     }),
   ],
   devServer: {
     // contentBase: './dist', //这个一般都不会去改的
     port: 666, //服务器端口
     open: true, //自动打开浏览器的窗口
+    hot: true,
     // 配置代理 处理跨域
     // proxy: {
     // 	'/api': {
