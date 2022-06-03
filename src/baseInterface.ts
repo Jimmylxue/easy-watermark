4ed1ec19af9a3bff4da57bc2c17e271368789972
jimmylxuexue <1002661758@qq.com>
2022-06-04 00:46:27 +0800

    :fire: 新增旋转角度

--- src/baseInterface.ts
+++ src/baseInterface.ts
@@ -3,40 +3,41 @@
  */
 
 export interface configParams {
-  src: string; // 图片地址
-  text: string; // 水印文字
-  size: number; // 水印文字字号
-  color: string; // 水印颜色
-  padding: number; // 文字距离图片的边距
-  output: string; // 输出类型 目前支持 png jpeg
-  position: string; // 水印方位
-  /**
-   * 水印方位
-   * left-top （左上）
-   * left-center （左中）
-   * left-botom （左下）
-   * center-top （中上）
-   * center （正中）
-   * center-bottom （中下）
-   * right-top （右上）
-   * right-center （右中）
-   * right-bottom （右下）
-   */
+	src: string // 图片地址
+	text: string // 水印文字
+	size: number // 水印文字字号
+	color: string // 水印颜色
+	padding: number // 文字距离图片的边距
+	output: string // 输出类型 目前支持 png jpeg
+	position: string // 水印方位
+	rotate?: number // 角度
+	/**
+	 * 水印方位
+	 * left-top （左上）
+	 * left-center （左中）
+	 * left-botom （左下）
+	 * center-top （中上）
+	 * center （正中）
+	 * center-bottom （中下）
+	 * right-top （右上）
+	 * right-center （右中）
+	 * right-bottom （右下）
+	 */
 }
 
 export interface textBound {
-  width: number;
-  height: number;
+	width: number
+	height: number
 }
 
 export const enum Position {
-  "left-top" = 1,
-  "left-center" = 2,
-  "left-bottom" = 3,
-  "center-top" = 4,
-  "center" = 5,
-  "center-bottom" = 6,
-  "right-top" = 7,
-  "right-center" = 8,
-  "right-bottom" = 9,
+	LEFT_TOP = 1,
+	LEFT_CENTER,
+	LEFT_BOTTOM,
+	CENTER_TOP,
+	CENTER,
+	CENTER_BOTTOM,
+	RIGHT_TOP,
+	RIGHT_CENTER,
+	RIGHT_BOTTOM,
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,48 +1,48 @@
-import { textBound, Position } from "./baseInterface";
+import { textBound, Position } from './baseInterface'
 
 // 获取文字的宽高
 export const getTextBound = (
-  ctx: CanvasRenderingContext2D,
-  font: string
+	ctx: CanvasRenderingContext2D,
+	font: string
 ): textBound => {
-  let baseMsg = ctx.measureText(font);
-  return {
-    width: baseMsg.width,
-    height: baseMsg.actualBoundingBoxDescent,
-  };
-};
+	let baseMsg = ctx.measureText(font)
+	return {
+		width: baseMsg.width,
+		height: baseMsg.actualBoundingBoxDescent,
+	}
+}
 
 // 获取方位
 export const getPositionType = (position): number => {
-  let flag = null;
-  switch (position) {
-    case "left-top":
-      flag = Position["left-top"];
-      break;
-    case "left-center":
-      flag = Position["left-center"];
-      break;
-    case "left-bottom":
-      flag = Position["left-bottom"];
-      break;
-    case "center-top":
-      flag = Position["center-top"];
-      break;
-    case "center":
-      flag = Position["center"];
-      break;
-    case "center-bottom":
-      flag = Position["center-bottom"];
-      break;
-    case "right-top":
-      flag = Position["right-top"];
-      break;
-    case "right-center":
-      flag = Position["right-center"];
-      break;
-    case "right-bottom":
-      flag = Position["right-bottom"];
-      break;
-  }
-  return flag;
-};
+	let flag = null
+	switch (position) {
+		case 'left-top':
+			flag = Position.LEFT_TOP
+			break
+		case 'left-center':
+			flag = Position.LEFT_CENTER
+			break
+		case 'left-bottom':
+			flag = Position.LEFT_BOTTOM
+			break
+		case 'center-top':
+			flag = Position.CENTER_TOP
+			break
+		case 'center':
+			flag = Position.CENTER
+			break
+		case 'center-bottom':
+			flag = Position.CENTER_BOTTOM
+			break
+		case 'right-top':
+			flag = Position.RIGHT_TOP
+			break
+		case 'right-center':
+			flag = Position.RIGHT_CENTER
+			break
+		case 'right-bottom':
+			flag = Position.RIGHT_BOTTOM
+			break
+	}
+	return flag
+}
